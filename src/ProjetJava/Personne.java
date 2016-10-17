f8ad27be7d75d1325be2769c13ee8be817b83e68
Goelzer <antoine.goelzer@isen-lille.fr>
2016-10-17 14:03:10 +0200

    Classe personne commencée

---
+++ src/ProjetJava/Personne.java
@@ -0,0 +1,22 @@
+package ProjetJava;
+
+import java.util.Date;
+
+/**
+ * Created by Antoine on 17/10/2016.
+ */
+public class Personne {
+    private String nomNaissance;
+    private String nomCourant;
+    private String prenom;
+    private String surnom;
+    private Date dateNaissance;
+    private String lieuNaissance;
+    private Date dateDeces;
+    private String nationalite;
+    private float taile;
+    private float poids;
+
+
+}
+
